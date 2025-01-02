c4be4a5ab6e834e473a73d341e73bf6d771c30a7
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-01-02 11:52:08 +0530

    Add files via upload

---
+++ C#/Console Applications/05.cs
@@ -0,0 +1,2 @@
+﻿int a = 5,b = 3;
+System.Console.WriteLine("a =" + a + "\t" + "b =" + b);
---
+++ C#/Console Applications/09.cs
@@ -0,0 +1,6 @@
+﻿int a = 5, b = 3;
+System.Console.WriteLine("Values before swap a=" + a + " b=" + b);
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+System.Console.WriteLine("Values after swap a=" + a + " b=" + b);
