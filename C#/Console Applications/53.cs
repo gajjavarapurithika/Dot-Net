3be0d0c8b4744ab34d80d12cb91758f225779304
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-01-19 13:02:45 +0530

    Add files via upload

---
+++ C#/Console Applications/53.cs
@@ -0,0 +1,10 @@
+﻿using System;
+int n,f = 1, i = 1;
+System.Console.WriteLine("enter a number:");
+n = Convert.ToInt32(Console.ReadLine());
+while(i<=n)
+{
+    f = f * i;
+    i = i + 1;
+}
+System.Console.WriteLine("Factorial of " + n + " is " + f);
