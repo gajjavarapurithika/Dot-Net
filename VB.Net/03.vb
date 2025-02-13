c1d8bcf68b4f167a34a7a714f4a0cf246df68582
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-13 11:39:07 +0530

    Create 03.vb

---
+++ VB.Net/03.vb
@@ -0,0 +1,9 @@
+Module Module1
+
+    Sub Main()
+        System.Console.WriteLine("Nipuna")
+        System.Console.WriteLine("VB.Net")
+        System.Console.WriteLine("Vijayawada")
+    End Sub
+
+End Module
