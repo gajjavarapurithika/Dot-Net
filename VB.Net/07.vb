4ad1b601f86d62b9db53e342a5a616e1bf74e079
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-14 11:00:16 +0530

    Create 07.vb

---
+++ VB.Net/07.vb
@@ -0,0 +1,15 @@
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 5
+        Dim b As Integer = 3
+        Dim c As Integer
+        System.Console.WriteLine("value of a=" & a & "b=" & b)
+        c = a
+        a = b
+        b = c
+        System.Console.WriteLine("Value of a=" & a & vbTab & & "b=" & b)
+
+    End Sub
+
+End Module
