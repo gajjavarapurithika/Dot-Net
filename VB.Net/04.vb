dd8f5ef955c7f8be1529f2b2bba4bdceb4a39da2
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-14 10:58:57 +0530

    Create 04.vb

---
+++ VB.Net/04.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    Sub Main()
+        Dim Age As Integer = 45
+        System.Console.Title = "My Code"
+        System.Console.WindowWidth = 300
+        System.Console.WindowHeight = 100
+        System.Console.WriteLine("Value of age" & Age)
+    End Sub
+
+End Module
