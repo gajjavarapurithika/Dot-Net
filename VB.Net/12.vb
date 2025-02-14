5076cc0c71f12d8b7fdd0024f96883d2e88a57d4
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-14 11:02:54 +0530

    Create 12.vb

---
+++ VB.Net/12.vb
@@ -0,0 +1,21 @@
+Module Module1
+
+    Sub Main()
+        Dim ecode As String = "E101"
+        Dim eno As Integer = 101
+        Dim ename As String = "Anil"
+        Dim esal As Double = 455.879
+        Dim egrade As String = "A"
+        Dim ews As Boolean = True
+        System.Console.WriteLine("------------------------------")
+        System.Console.WriteLine("Employee data")
+        System.Console.WriteLine("-------------------------------")
+        System.Console.WriteLine("Code=" & ecode)
+        System.Console.WriteLine("Number=" & eno)
+        System.Console.WriteLine("Salary=" & esal)
+        System.Console.WriteLine("Grade=" & egrade)
+        System.Console.WriteLine("Work status=" & ews)
+        System.Console.WriteLine("----------------------------------s")
+    End Sub
+
+End Module
