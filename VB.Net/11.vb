5f94428de4d37af6f7194ea28adbda7d21415a15
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-14 11:01:43 +0530

    Create 11.vb

---
+++ VB.Net/11.vb
@@ -0,0 +1,17 @@
+Imports System.Runtime.InteropServices
+
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 3
+        Dim b As Integer = 7
+        System.Console.WriteLine("Addition=" & (a + b))
+        System.Console.WriteLine("subtraction=" & (a - b))
+        System.Console.WriteLine("Multiplication=" & (a * b))
+        System.Console.WriteLine("Division=" & (a / b))
+        System.Console.WriteLine("Floor Division=" & (a \ b))
+        System.Console.WriteLine("Modulus=" & (a Mod b))
+        System.Console.WriteLine("Exponentional=" & (a ^ b))
+    End Sub
+
+End Module
