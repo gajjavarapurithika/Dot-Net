acbe593fa69b9097beb1bb22d98891503db282a8
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-14 10:59:50 +0530

    Create 06.vb

---
+++ VB.Net/06.vb
@@ -0,0 +1,14 @@
+Imports System.Runtime.InteropServices
+
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 5
+        Dim b As Integer = 7
+        System.Console.WriteLine("value of a=" & a)
+        System.Console.WriteLine("value of b=" & b)
+
+
+    End Sub
+
+End Module
