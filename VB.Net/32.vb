f64703edd452c8021b2851825ecf56be12c27b5e
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-18 09:08:24 +0530

    Create 32.vb

---
+++ VB.Net/32.vb
@@ -0,0 +1,12 @@
+Module Module1
+
+    Sub Main()
+        Dim i As Integer = 1
+        Do Until i > 10
+            System.Console.WriteLine(i)
+            i = i + 1
+        Loop
+
+    End Sub
+
+End Module
