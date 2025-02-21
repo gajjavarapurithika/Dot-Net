c46a7e26bb74b7d7c4594573f571c9deac7dc463
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-21 10:49:31 +0530

    Create 01.vb

---
+++ VB.Net/GUI Based Applications/01.vb
@@ -0,0 +1,9 @@
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        Me.BackColor = Color.LightBlue
+        Me.Text = "Hello World"
+        Me.Width = 300
+        Me.Height = 200
+    End Sub
+End Class
