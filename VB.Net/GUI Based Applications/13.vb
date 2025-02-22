aaece5c4e9ccea3a83a23aeebfea23f6c177677e
gajjavarapurithika <rithikagajjavarapu@gmail.com>
2025-02-22 11:12:05 +0530

    Create 13.vb

---
+++ VB.Net/GUI Based Applications/13.vb
@@ -0,0 +1,11 @@
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = " "
+    End Sub
+
+    Private Sub HScrollBar1_Scroll_1(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        Label1.Text = HScrollBar1.Value
+    End Sub
+End Class
